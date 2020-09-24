97874dab7bbaa0cafaab0871b5618824d8d04f60
Park-DongHo <ehdgh4875@gmail.com>
2020-09-25 04:16:08 +0900

    clear

--- yoloproject/yoloapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ImagePost, BookList, PetList
-
-# Register your models here.
-admin.site.register(ImagePost)
-admin.site.register(BookList)
-admin.site.register(PetList)
